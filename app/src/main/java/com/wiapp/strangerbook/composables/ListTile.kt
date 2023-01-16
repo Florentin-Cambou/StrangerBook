cef991632ffc8c6b21583f179ce1d87c6c2a2186
Florentin <florentin.cambou@gmail.com>
2023-01-16 16:25:27 +0100

    Add Icons and upgrade graphic interface

--- app/src/main/java/com/wiapp/strangerbook/composables/ListTile.kt
+++ app/src/main/java/com/wiapp/strangerbook/composables/ListTile.kt
@@ -1,17 +1,20 @@
 package com.wiapp.strangerbook.composables
 
+import android.widget.Space
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -56,10 +59,37 @@ fun ListTile(post: Post){
                 .clip(RoundedCornerShape(percent = 10))
         )
 
-        Row() {
-            //icon
+        Row(
+            modifier = Modifier.padding(start = 5.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Favorite,
+                contentDescription = null,
+                modifier = Modifier.padding(5.dp)
+            )
+            Icon(
+                imageVector = Icons.Default.Email,
+                contentDescription = null,
+                modifier = Modifier.padding(5.dp)
+            )
+            Icon(
+                imageVector = Icons.Default.Send,
+                contentDescription = null,
+                modifier = Modifier.padding(5.dp)
+            )
+            
+            Spacer(modifier = Modifier.weight(1.0F))
+            Icon(
+                imageVector = Icons.Default.Info,
+                contentDescription = null,
+                modifier = Modifier.padding(start = 5.dp)
+            )
         }
         Text(text = "${post.likes} J'aime")
-        Text(text = stringResource(id = post.description))
+        Text(
+            text = stringResource(id = post.description),
+            modifier = Modifier.padding(start = 10.dp)
+        )
+        Divider(modifier = Modifier.padding(5.dp))
     }
 }
